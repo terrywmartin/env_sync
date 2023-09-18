83e57b69bd4a61b78902b68d2953edcf5b6fae68
Terry Martin <terrywmartin@gmail.com>
2023-09-17 21:09:30 -0500

    added models to admin site

--- apikeys/admin.py
+++ apikeys/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import APIKeys
 # Register your models here.
+
+admin.site.register(APIKeys)
--- configfiles/admin.py
+++ configfiles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import ConfigFile, File, Location
 # Register your models here.
+admin.site.register(ConfigFile)
+admin.site.register(File)
+admin.site.register(Location)
